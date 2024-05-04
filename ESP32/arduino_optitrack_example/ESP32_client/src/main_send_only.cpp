13f2c290046a7f7cd47b167a4c5213d1255bf45d
sesteban951 <sesteban@caltech.edu>
2024-05-03 19:48:14 -0700

    added one way optitrack over ESP32

---
+++ ESP32/arduino_optitrack_example/ESP32_client/src/main_send_only.cpp
@@ -0,0 +1,219 @@
+/*
+PlatfromIO env params: 
+  Simple example as ESP32 client:
+  [env:featheresp32]
+  platform = espressif32
+  board = featheresp32
+  framework = arduino
+  monitor_speed = 115200
+
+Notes:
+  ssid: WIFI router name
+  password: WIFI router password
+  pc_inet: IP address of the server (laptop) to connect to
+           use "ifconfig" (linux) or "ipconfig" (windows) to get wlp inet addr on server from laptop (must do this)
+
+  1) ensure that number of  floats are agreed upon from server and client (i.e, adjust container sizes below)
+  2) run accopanying py_server_send_only.py script on server (on the laptop)
+  3) upload this script to ESP32, and run to send and receive from server (on the laptop)
+
+By Sergio Esteban (sesteban@caltech.edu)
+
+*/ 
+
+////////////////////////////////////// LIBRARIES //////////////////////////////////////
+
+// standrad arduino libraries
+#include <Arduino.h>
+#include <WiFi.h>
+
+// standrad C++ libraries for socket programming
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+////////////////////////////////////// VARIABLES //////////////////////////////////////
+
+// WIFI router information, must be modified when you change router or laptop
+const char* ssid = "NETGEAR46";
+const char* password = "amberlab";
+const char* pc_inet = "192.168.1.6"; // use "ifconfig"/"ipconfig" to get wlp inet addr on server from laptop
+
+// SOCKET server information
+#define PORT 8081
+int client_fd;   // socket file descriptor, an integer (like a file handle)
+int status;      // for connection error checking
+struct sockaddr_in serv_addr;
+int baud_rate = 115200;
+
+// Receiving message containers, Laptop --> ESP32
+const int recv_num_floats = 7;                    // change size as needed, each float is 4 bytes
+char recvBuffer[sizeof(float) * recv_num_floats]; // buffer for receviing data
+ssize_t bytes_read;                               // for meausring received packet size
+float x, y, z,        // position containers
+      qw, qx, qy, qz; // quaternion containers
+
+// TIMING variables
+double t0, t1;               // to measure time using internal ESP32 clock
+const float hz = 150.0;  // desired frequency of data transfer
+const float T = 1.0/hz;  // period of data transfer
+
+////////////////////////////////////// FUNCTIONS //////////////////////////////////////
+
+// function to setup the Wifi connection
+void setup_wifi(){
+
+  // WIFI_STA: Station mode (connect to wifi router/network)
+  // WIFI_AP: Access point mode (host wifi network)
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("\nConnecting to Wifi network...");
+
+  // Attempt to connect to the WiFi network
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(100);
+  }
+}
+
+// function to print info once the ESP32 is connected to the Wifi network
+void show_network_info(){
+
+  if(WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nSuccessfully to the WiFi network");
+
+    Serial.print("[*] Network information for ");
+    Serial.println(ssid);
+
+    Serial.println("[+] BSSID : " + WiFi.BSSIDstr());
+    Serial.print("[+] Gateway IP : ");
+    Serial.println(WiFi.gatewayIP());
+    Serial.print("[+] Subnet Mask : ");
+    Serial.println(WiFi.subnetMask());
+    Serial.println((String)"[+] RSSI : " + WiFi.RSSI() + " dB");
+    Serial.print("[+] ESP32 IP : ");
+    Serial.println(WiFi.localIP());
+
+    Serial.println("*****************************************");
+  }
+}
+
+// function to setup a client socket to connect to server over Wifi
+int setup_socket(){
+  
+  // create client socket with TCP (SOCK_STREAM), if you want UDP (SOCK_DGRAM)
+	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		Serial.print("\n Socket creation error \n");
+		return -1;
+	}
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+  Serial.println("Created socket.");
+
+  // set socket options
+	if (inet_pton(AF_INET, pc_inet, &serv_addr.sin_addr) <= 0) {
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+  Serial.println("Set inet options.");  
+
+  // attempt to connect to server
+  Serial.println("Attemptng to Connect to server..."); 
+	if ((status
+		= connect(client_fd, (struct sockaddr*)&serv_addr,
+				sizeof(serv_addr)))
+		< 0) {
+		Serial.print("Connection to Server Failed!\n");
+		return -1;
+	}
+  Serial.println("Connected to Server Successfully!");
+
+  return 0;
+}
+
+////////////////////////////////////// SETUP //////////////////////////////////////
+
+void setup(){
+
+  // setup serial
+  Serial.begin(baud_rate);
+  delay(100);
+
+  // enable LED_BUILTIN
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // setup wifi, connect to wifi router
+  setup_wifi();
+  delay(100);
+
+  // display all network info
+  show_network_info();
+  delay(100);
+  
+  // setup client socket
+  int socket_status = setup_socket();
+  delay(100);
+
+  // if socket setup failed, blink LED
+  if (socket_status == -1) {
+    Serial.println("Error setting up socket.");
+    while (true) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
+    }
+  }
+  // else, turn on solid LED to indicate successful socket setup
+  else {
+    Serial.println("Socket setup successful.");
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+  }
+  
+  t0 = micros();
+}
+
+////////////////////////////////////// LOOP //////////////////////////////////////
+
+void loop(){
+    
+    if (micros() - t0 >= (T * 1E6)) {
+    
+      t0 = micros();
+      
+      // Read the data into the buffer
+      bytes_read = read(client_fd, recvBuffer, sizeof(recvBuffer));
+
+      // Unpack the received byte stream into floats
+      memcpy(&x,    &recvBuffer[0 * sizeof(float)], sizeof(float));
+      memcpy(&y,    &recvBuffer[1 * sizeof(float)], sizeof(float));
+      memcpy(&z,    &recvBuffer[2 * sizeof(float)], sizeof(float));
+      memcpy(&qw,   &recvBuffer[3 * sizeof(float)], sizeof(float));
+      memcpy(&qx,   &recvBuffer[4 * sizeof(float)], sizeof(float));
+      memcpy(&qy,   &recvBuffer[5 * sizeof(float)], sizeof(float));
+      memcpy(&qz,   &recvBuffer[6 * sizeof(float)], sizeof(float));
+
+      // Use received data as needed
+      Serial.print("Received floats: (");
+      Serial.print(x ,4);
+      Serial.print(", ");
+      Serial.print(y, 4);
+      Serial.print(", ");
+      Serial.print(z, 4);
+      Serial.print(", ");
+      Serial.print(qw, 4);
+      Serial.print(", ");
+      Serial.print(qx, 4);
+      Serial.print(", ");
+      Serial.print(qy, 4);
+      Serial.print(", ");
+      Serial.print(qz, 4);
+      Serial.println(")");
+    }
+}
+
+
+
--- ESP32/arduino_optitrack_example/py_optitrack_server_send_only.py
+++ ESP32/arduino_optitrack_example/py_optitrack_server_send_only.py
@@ -1,9 +1,12 @@
-# example of a python server that communicates with a C++ client
-# 0. Connect your PC to the wifi router  via ethernet.
-# 1. In a terminal: roslaunch vrpn_client_ros sample.launch server:=<IPV4_of_optitrack_computer>
-# 2. Make sure that your rigid body name "topic_name" matches the one from motive in this script
-# 3. Launch this python script via: python3 py_optitrack_server.py
-# 4. Run the arduino code (ensure to pay attention to the instructiosn there too. I.e., SSID, PW, INET)
+# Example of a python server that communicates with a C++ client
+
+    # 0. Connect your PC to the wifi router  via ethernet.
+    # 1. In a terminal: roslaunch vrpn_client_ros sample.launch server:=<IPV4_of_optitrack_computer>
+    # 2. Make sure that your rigid body name "topic_name" matches the one from motive in this script.
+    #    I.e, topic_name = '/vrpn_client_node/<rigid-body-name>/pose'.
+    # 3. Launch this python script via: python3 py_optitrack_server_send_only.py
+    # 4. Run the arduino code (ensure to pay attention to the instructiosn there too. I.e., SSID, PW, INET)
+
 # By: Sergio Esteban (sesteban@caltech.edu)
 
 ############################################################################################
@@ -12,9 +15,6 @@ import socket # to use socket prgramming
 import struct # to use structs  when sending data
 import time   # to watch the communication frequency
 import numpy as np
-
-# import threading        # might need these to "multi thread" <-- one GIL 
-# import multiprocessing  # might need these to "multi thread" <-- multiple GILs
 
 import rospy
 from geometry_msgs.msg import PoseStamped
@@ -29,21 +29,17 @@ class OptiTrackServer:
         self.t0 = time.time()
 
         # send and receive variables
-        self.send_num_floats = 9
+        self.send_num_floats = 7
         self.send_f_str = 'f' * self.send_num_floats # number of f's is number of floats
 
-        self.recv_num_floats = 6
-        self.recv_f_str= 'f' * self.recv_num_floats
-
-        # stuff to send over drake system
-        self.esc  = np.zeros(2)  # right and left ESC
+        # stuff to send over Wifi
         self.pos  = np.zeros(3)  # position x, y, z
         self.quat = np.zeros(4)  # quaternion, qw, qx, qy, qz
 
         # ROS parameters
         node_name = 'optitrack_listener'
-        topic_name = '/vrpn_client_node/drone/pose'
-        hz = 300
+        topic_name = '/vrpn_client_node/harpy/pose'
+        hz = 150.0
 
         # setup ROS node
         rospy.init_node(node_name, anonymous=True)  # init ROS node
@@ -53,7 +49,6 @@ class OptiTrackServer:
         self.rate = rospy.Rate(hz)                  # set loop rate [hz]    
         rospy.sleep(1)                              # wait for subscriber to connect
 
-
     # get pose and twist data from OptiTrack
     def callback(self,data):
 
@@ -61,17 +56,8 @@ class OptiTrackServer:
         pos_world = data.pose.position
         quat_world = data.pose.orientation
 
-        self.esc[0] = self.sine_wave(0)
-        self.esc[1] = self.sine_wave(np.pi/4)
         self.pos = np.array([[pos_world.x], [pos_world.y], [pos_world.z]])
         self.quat = np.array([[quat_world.w], [quat_world.x], [quat_world.y], [quat_world.z]])
-    
-    # arbitrary function to generate data
-    def sine_wave(self, phi):
-        t = time.time() - self.t0
-        f = 0.5
-        x = 35.0 * np.sin(2 * np.pi * f * t + phi)
-        return  x
 
     # setup all the networking
     def setup_server_socket(self):
@@ -109,17 +95,12 @@ class OptiTrackServer:
 
             # send message to the client
             msg_to_client = struct.pack(self.send_f_str,
-                                        self.esc[0],  self.esc[1],
                                         self.pos[0],  self.pos[1],  self.pos[2],
                                         self.quat[0], self.quat[1], self.quat[2], self.quat[3])
             self.conn.send(msg_to_client)
 
-            # receive message from client
-            msg_from_client = self.conn.recv(self.recv_num_floats * 4)     # each float is 4 bytes
-            recvd_floats = struct.unpack(self.recv_f_str, msg_from_client)
-
             # print the received message
-            print("Received: ", recvd_floats)
+            print("Sent: ", self.pos[0], self.pos[1], self.pos[2], self.quat[0], self.quat[1], self.quat[2], self.quat[3])
 
             self.rate.sleep()
 
@@ -129,4 +110,3 @@ if __name__ == '__main__':
     
     opti_serve = OptiTrackServer()
     opti_serve.server_program()
-
